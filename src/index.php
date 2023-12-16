4feeda05ae2f358a169a5567087118095a03abe7
Yasmeen <148520658+HalioOoO@users.noreply.github.com>
2023-12-16 15:15:07 +0800

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+	  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<?php
+
+	echo "<h1>Alhamdulillah</h2>";
+	
+?>
+
+</body>
+</html>
+
